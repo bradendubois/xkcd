84af53012c621f86fe43d0a931f718c1bc24f02f
Braden Dubois <braden.dubois@usask.ca>
2021-08-29 22:38:53 -0600

    parser working

--- xkcd/__main__.py
+++ xkcd/__main__.py
@@ -7,32 +7,41 @@ from argparse import ArgumentParser
 from bs4 import BeautifulSoup
 from os import name, path, makedirs
 from requests import get
-from sys import argv
-from time import sleep
-from typing import Tuple
-from urllib import request
+from typing import Optional, Tuple
 from PIL import Image
+from io import BytesIO
+from re import match
 
 
-def get_comic() -> Tuple[str, Image.Image]:
+def get_comic(comic: Optional[int]) -> Tuple[str, Image.Image]:
 
-    request = get(f"https://xkcd.com").text.strip()
+    code = comic if comic else ""
+
+    request = get(f"https://xkcd.com/{code}").text.strip()
     soup = BeautifulSoup(request, "html.parser")
 
+    print("Title:", soup.find("div", {"id": "ctitle"}).text)
+
     comic = soup.find("div", {"id": "comic"})
-    print(comic)
+    img = comic.find("img")
+
+    print("Message:", img["title"])
+
+    url = next(link for link in soup.findAll("a") if match('.*/xkcd.com/[0-9]*', link["href"]))["href"]
+    print("URL:", url)
+
+    content = get(f"https://{img['src'].lstrip('/')}").content
+    image = Image.open(BytesIO(content))
+    image.show()
 
 
 def run():
 
     parser = ArgumentParser(prog="xkcd")
+    parser.add_argument("--comic", type=int)
 
     namespace = parser.parse_args()
-
-    get_comic()
-
-    # title, image =
-
+    get_comic(namespace.comic)
 
 
 if __name__ == "__main__":
